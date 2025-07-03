e116e553e2542be161f2a7443d9348ef7fbabb88
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-07-03 20:49:50 +0530

    fixing bug

--- db/db_config.py
+++ db/db_config.py
@@ -2,13 +2,13 @@ import mysql.connector
 from mysql.connector import Error
 
 def get_connection():
-    conn=mysql.connector.connection(
+    return (mysql.connector.connection(
     host="localhost",
     user="root",
     password="devanshsrm@2027",
     database="student_db"
-    )
-    return conn
+    ))
+
     
     
     
